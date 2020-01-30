f289d3c7de60de98d03a34a34de9a0c634807f3c
Addroc12 <adityanrnsingh270@gmail.com>
2020-01-30 13:04:57 +0530

    First commit

---
+++ final_class.java
@@ -0,0 +1,14 @@
+
+// final class.
+// you can't inherit the final class.
+class first 
+{
+	
+}
+class second extends first
+{
+	public static void main(String args[])
+	{	
+		System.out.println("I  am into second class.");
+	}
+}
